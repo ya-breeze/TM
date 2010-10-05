48f287c2d90555efc0fb5a9e4937e78b8cf14c29
Ilia Korolyov <ya-breeze@ya.ru>
2010-10-05 22:40:48 +0400

    exec sql

--- test/dbaccess.cpp
+++ test/dbaccess.cpp
@@ -2,6 +2,7 @@
 
 #include <stdexcept>
 
+#include <QVariant>
 #include <QSqlError>
 #include <QSqlQuery>
 #include <QProcessEnvironment>
@@ -22,10 +23,29 @@ DbAccess::DbAccess()
 }
 
 Task::TaskList DbAccess::getTaskList(const QUuid& _parent) {
-    return Task::TaskList();
+    Task::TaskList result;
+
+    QSqlQuery query;
+    query.prepare("SELECT uuid, name FROM Tasks WHERE parentUuid=?;");
+    query.addBindValue(QVariant(_parent.toString()));
+    if( !query.exec() )
+        throw std::runtime_error(m_Db.lastError().text().toStdString());
+    while (query.next()) {
+        QString uuid = query.value(0).toString();
+        QString name = query.value(1).toString();
+//        qDebug() << uuid << name;
+    }
+
+    return result;
 }
 
 void DbAccess::addTask(const Task& _task) {
     QSqlQuery query;
-    query.exec("SELECT name, salary FROM employee WHERE salary > 50000");
+    query.prepare("INSERT INTO Tasks(uuid, parentUuid, name) VALUES(?, ?, ?);");
+    query.addBindValue(_task.getUuid().toString());
+    query.addBindValue(_task.getParentUuid().toString());
+    query.addBindValue(_task.getName());
+
+    if( !query.exec() )
+        throw std::runtime_error(m_Db.lastError().text().toStdString());
 }
--- test/mainwindow.cpp
+++ test/mainwindow.cpp
@@ -6,6 +6,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    for(int i=0; i<1000; ++i) {
+        Task task;
+        task.setName("test name");
+        task.setUuid( QUuid::createUuid() );
+
+        m_Tasks.addTask(task);
+    }
 }
 
 MainWindow::~MainWindow()
